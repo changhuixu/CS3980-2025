58171aaa0bb58ee35822a5dc2929bf4cacf6e28f
Changhui Xu <changhuixu@hotmail.com>
2025-02-27 13:16:25 -0600

    update todo_router by adding more api endpoints

--- module06/todo_routes.py
+++ module06/todo_routes.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, status
+from typing import Annotated
+from fastapi import APIRouter, HTTPException, Path, status
 
 from todo import Todo
 
@@ -14,6 +15,30 @@ async def get_todos() -> dict:
 
 
 @todo_router.post("", status_code=status.HTTP_201_CREATED)
-async def add_todo(todo: Todo) -> dict:
+async def add_todo(todo: Todo) -> Todo:
     todo_list.append(todo)
-    return {"message": "new todo added"}
+    return todo
+
+
+@todo_router.get("/{id}")
+async def get_todo_by_id(id: Annotated[int, Path(ge=0, le=1000)]) -> Todo:
+    for todo in todo_list:
+        if todo.id == id:
+            return todo
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail=f"Item with ID={id} is not found"
+    )
+
+
+@todo_router.delete("/{id}")
+async def delete_todo_by_id(id: Annotated[int, Path(ge=0, le=1000)]) -> dict:
+    for i in range(len(todo_list)):
+        todo = todo_list[i]
+        if todo.id == id:
+            todo_list.pop(i)
+            return {"msg": f"The todo with ID={id} is removed."}
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail=f"Item with ID={id} is not found"
+    )
