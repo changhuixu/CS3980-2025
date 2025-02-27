98d66b86b58a77bc9d3299d762a357c035f76af8
Changhui Xu <changhuixu@hotmail.com>
2025-02-27 13:42:33 -0600

    first implementation of frontend

--- module06/main.py
+++ module06/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 from todo_routes import todo_router
 
 app = FastAPI(title="My Todo App")
@@ -7,4 +9,7 @@ app.include_router(todo_router, tags=["Todos"], prefix="/todos")
 
 @app.get("/")
 async def welcome() -> dict:
-    return {"msg": " Hello"}
+    return FileResponse("./frontend/index.html")
+
+
+app.mount("/", StaticFiles(directory="frontend"), name="static")
