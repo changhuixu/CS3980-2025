c5d4d917327bbf412d008efcf3b2452d0fbb2541
Changhui Xu <changhuixu@hotmail.com>
2025-03-06 13:26:57 -0600

    add a button and modal for adding todo

--- module06/frontend/main.js
+++ module06/frontend/main.js
@@ -1,5 +1,30 @@
 const api = 'http://127.0.0.1:8000/todos';
 
+document.getElementById('save-new-todo').addEventListener('click', (e) => {
+  e.preventDefault();
+  postTodo();
+  const closeBtn = document.getElementById('add-close');
+  closeBtn.click();
+});
+
+const postTodo = () => {
+  const titleInput = document.getElementById('new-title');
+  const title = titleInput.value;
+  const descInput = document.getElementById('new-desc');
+  const desc = descInput.value;
+
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState == 4 && xhr.status == 201) {
+      getTodos();
+    }
+  };
+
+  xhr.open('POST', api, true);
+  xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+  xhr.send(JSON.stringify({ title, desc }));
+};
+
 const displayTodos = (todos) => {
   const tbody = document.getElementById('todo-rows');
   tbody.innerHTML = '';
